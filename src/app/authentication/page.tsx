fe432858bde8d435d6a171ed33db52cbfd7cb649
Pedro henrique yudi kawamoto <pedroyudi78@gmail.com>
2026-03-02 14:38:31 -0300

    feat create route to login and situp

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,10 +1,18 @@
-'use client'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import React from 'react'
 import SingUpForm from './components/sing-up-form';
 import LoginForm from './components/login-form';
+import { redirect } from 'next/navigation';
+import { auth } from '@/lib/auth';
+import { headers } from 'next/headers';
 
-const AuthenticationPage = () => {
+const AuthenticationPage =  async () => {
+  const session = await auth.api.getSession({
+          headers: await headers(),
+      })
+      if(session?.user){
+        redirect("/dashboard")
+      }
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <Tabs defaultValue="login" className="w-100">
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,25 @@
-const DashBoardPage = () =>{
-    return(
-        <div>
-            <h1>DashBoard</h1>
-        </div>
-    )
+import { auth } from "@/lib/auth"
+import { headers } from "next/headers"
+import SigOutButton from "./_components/sign-out-button"
+import { redirect } from "next/navigation"
+
+const DashBoardPage = async () =>{
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    })
+    if(!session){
+      redirect("/authentication")
+    }
+    return (
+      <div>
+        <h1>DashBoard</h1>
+        <h1>{session?.user.name}</h1>
+        <h1>{session?.user.email}</h1>
+        <SigOutButton/>
+      </div>
+    );
 }
 
 export default DashBoardPage
+
+// Renderiza a saida com useClient
